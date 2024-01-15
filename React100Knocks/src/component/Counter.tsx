7ace9970219f08b244ec2191e26a71f03a58815e
yoshikawa masashi <gunterriote@yahoo.co.jp>
2024-01-16 06:53:27 +0900

    デザインを修正

--- React100Knocks/src/component/Counter.tsx
+++ React100Knocks/src/component/Counter.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Spacer } from "./Spacer";
 
 function Counter() {
     const [count, setCount] = useState(0);
@@ -8,12 +9,16 @@ function Counter() {
     const handleMinusClick = () => {
         setCount(count - 1);
     };
-    return <p className="max-w-sm rounded overflow-hidden shadow-lg">
-        <div className="text-3xl font-bold underline">React Counter</div>
-        <span>{count}</span>
+    return <p className="container rounded overflow-hidden shadow-lg bg-white">
+        <div className="text-gray-300 text-3xl font-bold underline px-3 py-1">React Counter</div>
+        <Spacer size={60} />
+        <span className="text-blue-600 font-bold text-6xl">{count}</span>
+        <Spacer size={60} />
         <div>
             <button className="bg-blue-500 hover:bg-blue-400 text-white rounded px-3 py-1" onClick={handlePlusClick}>+</button>
+            <Spacer size={100} horizontal={true}/>
             <button className="bg-blue-500 hover:bg-blue-400 text-white rounded px-3 py-1" onClick={handleMinusClick}>-</button>
+            <Spacer size={30}/>
         </div>
     </p>;
 }
