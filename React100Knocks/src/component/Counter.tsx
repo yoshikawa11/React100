6d2b47bb03a4bf041bc0ab1194a8c95f1d5f9291
yoshikawa masashi <gunterriote@yahoo.co.jp>
2024-01-15 20:02:03 +0900

    tailwindcssを実装

--- React100Knocks/src/component/Counter.tsx
+++ React100Knocks/src/component/Counter.tsx
@@ -8,12 +8,12 @@ function Counter() {
     const handleMinusClick = () => {
         setCount(count - 1);
     };
-    return <p>
-        <div>React Counter</div>
+    return <p className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="text-3xl font-bold underline">React Counter</div>
         <span>{count}</span>
         <div>
-            <span onClick={handlePlusClick}>+</span>
-            <span onClick={handleMinusClick}>-</span>
+            <button className="bg-blue-500 hover:bg-blue-400 text-white rounded px-3 py-1" onClick={handlePlusClick}>+</button>
+            <button className="bg-blue-500 hover:bg-blue-400 text-white rounded px-3 py-1" onClick={handleMinusClick}>-</button>
         </div>
     </p>;
 }
